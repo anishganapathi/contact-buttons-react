d0d3c8f1a7ac3a8e70315f7b372784e794fb5a8e
akash <anishganapathi19@gmail.com>
2023-09-20 12:33:42 +0530

    Changes in buttons
    + Added tailwind cards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Contact from './Contact';
 import './App.css';
+import CardWithLink from './components/CardWithLink';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <CardWithLink />
       <Contact />
       </header>
     </div>
